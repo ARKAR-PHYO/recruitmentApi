63e3a06343b98b1a49ca18f5ecfe4db406b77125
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2021-09-26 16:33:07 +0630

    refind recruit category

--- app/Http/Controllers/RecruitCategoriesController.php
+++ app/Http/Controllers/RecruitCategoriesController.php
@@ -7,75 +7,16 @@ use Illuminate\Http\Request;
 
 class RecruitCategoriesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function getAllCategories()
     {
         return RecruitCategory::orderBy('id', 'desc')->get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request, RecruitCategory $recruitCategory)
+    public function createRecruitCategory(Request $request, RecruitCategory $recruitCategory)
     {
         return $recruitCategory->create($request->validate([
             'category_name' => 'required',
             'category_slug' => 'bail|required|unique:recruit_categories,category_slug',
         ]));
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($category_slug, RecruitCategory $recruitCategory)
-    {
-        return $recruitCategory->where('category_slug', $category_slug)->firstOrFail();
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $category_slug, RecruitCategory $recruitCategory)
-    {
-        return $recruitCategory->where('category_slug', $category_slug)->update($request->validate([
-            'category_name' => 'required',
-            'category_slug' => "bail|required|unique:recruit_categories,category_slug,$request->id"
-        ]));
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($category_slug, RecruitCategory $recruitCategory)
-    {
-        return $recruitCategory->where('category_slug', $category_slug)->delete();
-    }
-
-     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  str  $category_name
-     * @return \Illuminate\Http\Response
-     */
-    public function search($category_name)
-    {
-        return RecruitCategory::where('category_name', 'like', '%'.$category_name.'%')->get();
     }
 }
